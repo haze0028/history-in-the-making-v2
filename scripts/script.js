19f023d0832c013bf17186706c10a401ddc948e1
Greg Hazelton <haze0028@algonquinlive.com>
2018-04-11 16:33:32 -0400

    general styles + navbars

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,31 @@
 (function () {
-   console.log("Page loaded");
-
-   $('nav li').hover(function(){
-      $(this).find('ul').slideToggle('fast');
-   })
+	console.log("Page loaded");
+	var wrapper, toggle, nav;
 	
+	// trigger nav menu dropdown effects
+	$('nav li').hover(function () {
+		$(this).find('ul').slideToggle('fast');
+	})
+
+	//side menu slide in effect
+	wrapper = $(".wrap");
+	toggle = $(".toggle");
+	nav = $(".side-nav");
+	
+	toggle.on("click", function () {
+		wrapper.toggleClass("nav-open");
+		// Change the font-awesome icons on click.
+		toggle.toggleClass("fa-bars");
+		toggle.toggleClass("fa-times");
+	});
+
+	$(window).on("click", function (e) {
+		if (wrapper.hasClass("nav-open") &&
+			!$(e.target).hasClass("toggle")) {
+			wrapper.removeClass("nav-open");
+			toggle.toggleClass("fa-bars");
+			toggle.toggleClass("fa-times");
+		}
+	});
+
 })();
