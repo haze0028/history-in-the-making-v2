f527a0a3d4406e5543a0734bb4b5d445a8ac2788
Greg Hazelton <haze0028@algonquinlive.com>
2018-04-25 16:40:16 -0400

    nav update

--- scripts/script.js
+++ scripts/script.js
@@ -14,10 +14,12 @@
 		$(this).toggleClass('flipped-down').toggleClass('flipped');
 		$(this).parent().find('ul').slideToggle();
 	})
-	
-	$('.toggle-menu').on('blur', function(){
-		$(this).toggleClass('flipped').toggleClass('flipped-down');
-		$(this).parent().find('ul').slideUp();		
+
+	$('.toggle-menu').on('blur', function () {
+		if ($(this).hasClass('flipped')){
+			$(this).toggleClass('flipped').toggleClass('flipped-down');
+			$(this).parent().find('ul').slideUp();
+		}
 	})
 
 	// trigger nav menu dropdown effects
@@ -120,61 +122,61 @@
 	/////////////
 	//Flip images
 	////////////
-	$('.clothing-items .main-img').on('click', function(){
+	$('.clothing-items .main-img').on('click', function () {
 		$(this).toggleClass('show-img').toggleClass('hide-img');
 		$(this).parent().find($('.alt-img')).toggleClass('hide-img').toggleClass('show-img');
 	})
-	$('.clothing-items .alt-img').on('click', function(){
+	$('.clothing-items .alt-img').on('click', function () {
 		$(this).toggleClass('show-img').toggleClass('hide-img');
 		$(this).parent().find($('.main-img')).toggleClass('hide-img').toggleClass('show-img');
 	})
-	
+
 
 	/////////////
 	//Jquey media queries
 	////////////
-	
-//	$(window).resize(function(){
-//		//Below 1200 VW
-//	if ($(window).width() <= 1200){	
-//		console.log('changing');
-//		$('.top-btn').find('i').removeClass('fa-arrow-circle-up').addClass('fas fa-arrow-up');
-//	}	else{ //Above 1200
-//		$('.top-btn').find('i').removeClass('fas fa-arrow-up').addClass('fa-arrow-circle-up');
-//		
-//	}
-//});
-	
-//	// run test on initial page load
-//	checkSize();
-//	var responsiveValue = $('#responsive').css("top");
-//	// run test on resize of the window
-//	$(window).resize(checkSize);
-//
-//	//Function to the css rule
-//	function checkSize() {
-//		console.log((responsiveValue));
-//		switch (responsiveValue) {
-//			case 5:
-//				console.log("lg desktop");
-//				break;
-//			case 4:
-//				console.log("md desktop");
-//				break;
-//			case 3:
-//				console.log("tablet");
-//				break;
-//			case 2:
-//				console.log("lg phone");
-//				break;
-//			case 1:
-//				console.log("sm phone");
-//				break;
-//			default:
-//				console.log("woops");
-//				break;
-//		}
-//	}
+
+	//	$(window).resize(function(){
+	//		//Below 1200 VW
+	//	if ($(window).width() <= 1200){	
+	//		console.log('changing');
+	//		$('.top-btn').find('i').removeClass('fa-arrow-circle-up').addClass('fas fa-arrow-up');
+	//	}	else{ //Above 1200
+	//		$('.top-btn').find('i').removeClass('fas fa-arrow-up').addClass('fa-arrow-circle-up');
+	//		
+	//	}
+	//});
+
+	//	// run test on initial page load
+	//	checkSize();
+	//	var responsiveValue = $('#responsive').css("top");
+	//	// run test on resize of the window
+	//	$(window).resize(checkSize);
+	//
+	//	//Function to the css rule
+	//	function checkSize() {
+	//		console.log((responsiveValue));
+	//		switch (responsiveValue) {
+	//			case 5:
+	//				console.log("lg desktop");
+	//				break;
+	//			case 4:
+	//				console.log("md desktop");
+	//				break;
+	//			case 3:
+	//				console.log("tablet");
+	//				break;
+	//			case 2:
+	//				console.log("lg phone");
+	//				break;
+	//			case 1:
+	//				console.log("sm phone");
+	//				break;
+	//			default:
+	//				console.log("woops");
+	//				break;
+	//		}
+	//	}
 
 
 })();
